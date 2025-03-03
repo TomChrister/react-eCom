eb5b953280753e20e64949881282f79ab79c79b3
TOMCHRISTER\tomch <tomsch01266@stud.noroff.no>
2025-03-03 15:07:47 +0100

    feat: added flowbite

--- layout/Header.jsx
+++ layout/Header.jsx
@@ -5,21 +5,23 @@ export function Header() {
     const { products } = useCart();
 
     return (
-        <header>
-            <nav className='flex justify-between px-3'>
-                <div className='flex gap-2'>
-                    <Link to='/'>Home</Link>
-                    <Link to='/contact'>Contact</Link>
-                </div>
-                <Link to='/checkout'>
-                    <img src='../public/cart.svg' alt='Cart Image' className='w-6'/>
-                    {products.length > 0 && (
-                        <span className='absolute top-1 -right-0 text-md rounded-full px-1'>
+        <>
+            <header>
+                <nav className='flex justify-between px-3'>
+                    <div className='flex gap-2'>
+                        <Link to='/'>Home</Link>
+                        <Link to='/contact'>Contact</Link>
+                    </div>
+                    <Link to='/checkout'>
+                        <img src='../public/cart.svg' alt='Cart Image' className='w-6'/>
+                        {products.length > 0 && (
+                            <span className='absolute top-1 -right-0 text-md rounded-full px-1'>
                             {products.length}
                         </span>
-                    )}
-                </Link>
-            </nav>
-        </header>
+                        )}
+                    </Link>
+                </nav>
+            </header>
+        </>
     )
 }
